1f2abf74e7169e277c48eeaa341da1fedfc51158
mary-anitikin <Maria Nikitina>
2023-09-28 16:54:48 +0300

    Создание связей между моделями через класс ForeignKey

--- women/urls.py
+++ women/urls.py
@@ -4,8 +4,14 @@ from .views import *
  
 urlpatterns = [
     path('', index, name='home'),
-    path('cats/', categoriesone, name='cats'),
-    path('cats/<slug:cat>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive, name='archive'),
     path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+#    path('cats/', categoriesone, name='cats'),
+#    path('cats/<slug:cat>/', categories),
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive, name='archive'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
+
--- women/views.py
+++ women/views.py
@@ -4,11 +4,23 @@ from .models import *
 
 # Create your views here.
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'},
+]
 
 def index(request):
    posts = Women.objects.all()
-   return render(request, 'index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+   cats = Category.objects.all()
+   context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0
+   } 
+   return render(request, 'index.html', context=context)
 
 def categories(request, cat):
     print(request.GET)
@@ -27,3 +39,29 @@ def pageNotFound(request, exception):
 
 def about(request):
     return render(request, 'about.html', {'menu': menu, 'title': 'О сайте'})
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    if len(posts) == 0:
+        raise Http404()
+    cats = Category.objects.all() 
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': cat_id,
+    } 
+    return render(request, 'index.html', context=context)
